e86179d7df13ccb05f671e604990338fa2fa0fd4
JaydenHuang555 <JaydenKanaHuang@outlook.com>
2026-03-23 23:13:14 -0700

    actions with clap

--- fetchcmd/src/main.rs
+++ fetchcmd/src/main.rs
@@ -1,7 +1,7 @@
 pub mod options;
 pub mod proj_dir;
 
-// use directories::ProjectDirs;
+use directories::ProjectDirs;
 use fetchlib::key::Secrets;
 use rpassword::read_password;
 
@@ -11,19 +11,7 @@ use fetchlib::client::Client;
 use crate::options::Options;
 
 fn main() {
-    // let proj_directory = ProjectDirs::from("com", "Jayden", "fetch").unwrap();
-
+    let proj_directory = ProjectDirs::from("com", "Jayden", "fetch").unwrap();
     let options = Options::parse();
-
-    let pass_read = read_password().unwrap();
-
-    let pass = Secrets::get_pass(pass_read);
-
-    let inputs = options.convert_to_inputs(pass);
-
-    let mut client = Client::spawn(inputs).unwrap();
-
-    let output = client.run_cmd("cd ~; ls ");
-    println!("Exit Code: {}", output.0);
-    println!("Content: {}", output.1);
+    options.action.execute();
 }
