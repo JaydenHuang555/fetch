f1ca624f5cbc7abbacd5ce93998c30519bfcbddc
JaydenHuang555 <JaydenKanaHuang@outlook.com>
2026-03-23 23:53:04 -0700

    constants

---
+++ fetchcmd/src/constants.rs
@@ -0,0 +1,29 @@
+use std::{
+    path::{Path, PathBuf},
+    sync::Mutex,
+};
+
+use once_cell::sync::Lazy;
+
+use crate::proj_dir::PROJECT_INSTANCE;
+
+pub struct Constants {
+    pub profiles_path: PathBuf,
+    pub cache_path: PathBuf,
+}
+
+lazy_static::lazy_static! {
+
+    pub static ref INSTANCE: Lazy<Mutex<Constants>> = Lazy::new(|| {
+        let proj = PROJECT_INSTANCE.lock().unwrap().dir.clone();
+        let profiles_path = proj.config_dir().join("profiles");
+        let cache_path = proj.cache_dir().to_path_buf();
+        Mutex::new(
+            Constants {
+                profiles_path: profiles_path,
+                cache_path: cache_path
+            }
+        )
+    });
+
+}
