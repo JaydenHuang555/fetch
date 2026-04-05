e1b5038dbd6ee810ff3bee001231039342fb5938
JaydenHuang555 <JaydenKanaHuang@outlook.com>
2026-04-04 22:43:35 -0700

    utils to avoid repeatitive code

---
+++ fetchlib/src/util/ssh2/macros.rs
@@ -0,0 +1,17 @@
+macro_rules! remote_secure_shell_channel_close {
+    ($remote_channel:expr) => {{
+        if let Err(e) = $remote_channel.send_eof() {
+            Some(e)
+        } else if let Err(e) = $remote_channel.wait_eof() {
+            Some(e)
+        } else if let Err(e) = $remote_channel.close() {
+            Some(e)
+        } else if let Err(e) = $remote_channel.wait_close() {
+            Some(e)
+        } else {
+            None
+        }
+    }};
+}
+
+pub(crate) use remote_secure_shell_channel_close;
--- fetchlib/src/util/ssh2/mod.rs
+++ fetchlib/src/util/ssh2/mod.rs
@@ -1,3 +1,5 @@
 pub mod error;
+pub mod macros;
 
 pub use error::Error;
+pub(crate) use macros::remote_secure_shell_channel_close;
